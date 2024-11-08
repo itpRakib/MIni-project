6f82bb3449dda4c9f59bcf849857db79eafbb03d
itpRakib <rakibulislam.cse.41230301525@gmail.com>
2024-11-09 00:01:40 +0600

    n

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const accordions = document.querySelectorAll('.accordion');
-accordions.forEach(accordion =>{
+
+accordions.forEach(accordion => {
     const icon = accordion.querySelector('.icon');
     const answer = accordion.querySelector('.answer');
 
     accordion.addEventListener('click', () => {
-        icon.classList.toggle('active');
-        answer.classList.toggle('active');
+
+        if (icon.classList.contains('active')) {
+            icon.classList.remove('active');
+            answer.style.maxHeight = null;
+        }
+        else {
+            icon.classList.add('active');
+            answer.style.maxHeight = answer.scrollHeight + 'px';
+        }
     })
 })
